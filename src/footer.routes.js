26086b6c76f26f223912136ee90a2497d5d1fca7
unknown <kevin.cuanz18@gmail.com>
2023-02-04 21:35:08 +0700

    Change footer

--- src/footer.routes.js
+++ src/footer.routes.js
@@ -1,91 +1,50 @@
-// @mui icons
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import YouTubeIcon from "@mui/icons-material/YouTube";
-
-// Material Kit 2 React components
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import MKTypography from "components/MKTypography";
 
 // Images
 import logoCT from "assets/images/logo-ct-dark.png";
 
-const date = new Date().getFullYear();
+// const date = new Date().getFullYear();
 
 export default {
   brand: {
-    name: "Material Kit 2",
+    name: "TechnoBend",
     image: logoCT,
     route: "/",
   },
   socials: [
     {
       icon: <FacebookIcon />,
-      link: "https://www.facebook.com/CreativeTim/",
+      link: "https://www.facebook.com/people/Technobend-Indonesia/100063777990557/",
     },
     {
       icon: <TwitterIcon />,
-      link: "https://twitter.com/creativetim",
+      link: "https://twitter.com/technobend",
     },
     {
-      icon: <GitHubIcon />,
-      link: "https://github.com/creativetimofficial",
-    },
-    {
-      icon: <YouTubeIcon />,
-      link: "https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w",
+      icon: <LinkedInIcon />,
+      link: "https://www.linkedin.com/in/technobend-indonesia-63617929/?originalSubdomain=id",
     },
   ],
   menus: [
     {
-      name: "company",
-      items: [
-        { name: "about us", href: "https://www.creative-tim.com/presentation" },
-        { name: "freebies", href: "https://www.creative-tim.com/templates/free" },
-        { name: "premium tools", href: "https://www.creative-tim.com/templates/premium" },
-        { name: "blog", href: "https://www.creative-tim.com/blog" },
-      ],
+      name: "Event",
+      items: [{ name: "All event", href: "https://www.creative-tim.com/presentation" }],
     },
     {
-      name: "resources",
+      name: "contact us",
       items: [
-        { name: "illustrations", href: "https://iradesign.io/" },
-        { name: "bits & snippets", href: "https://www.creative-tim.com/bits" },
-        { name: "affiliate program", href: "https://www.creative-tim.com/affiliates/new" },
-      ],
-    },
-    {
-      name: "help & support",
-      items: [
-        { name: "contact us", href: "https://www.creative-tim.com/contact-us" },
-        { name: "knowledge center", href: "https://www.creative-tim.com/knowledge-center" },
-        { name: "custom development", href: "https://services.creative-tim.com/" },
-        { name: "sponsorships", href: "https://www.creative-tim.com/sponsorships" },
-      ],
-    },
-    {
-      name: "legal",
-      items: [
-        { name: "terms & conditions", href: "https://www.creative-tim.com/terms" },
-        { name: "privacy policy", href: "https://www.creative-tim.com/privacy" },
-        { name: "licenses (EULA)", href: "https://www.creative-tim.com/license" },
+        { name: "Office", href: "https://www.creative-tim.com/terms" },
+        { name: "Factory", href: "https://www.creative-tim.com/privacy" },
+        { name: "Send us a message", href: "https://www.creative-tim.com/license" },
       ],
     },
   ],
   copyright: (
     <MKTypography variant="button" fontWeight="regular">
-      All rights reserved. Copyright &copy; {date} Material Kit by{" "}
-      <MKTypography
-        component="a"
-        href="https://www.creative-tim.com"
-        target="_blank"
-        rel="noreferrer"
-        variant="button"
-        fontWeight="regular"
-      >
-        Creative Tim
-      </MKTypography>
-      .
+      &copy; 2023 Technobend | Induction Bend and BW Fitting Manufacturer, All right reserved.{" "}
     </MKTypography>
   ),
 };
