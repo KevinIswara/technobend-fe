582d952ce6afab2cccbee5d332f571b562c86f4d
unknown <kevin.cuanz18@gmail.com>
2023-02-09 21:58:16 +0700

    Create Induction Bending page

---
+++ src/pages/Products/InductionBending/index.js
@@ -0,0 +1,35 @@
+import Card from "@mui/material/Card";
+import MKBox from "components/MKBox";
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+import DefaultFooter from "examples/Footers/DefaultFooter";
+import routes from "routes";
+import footerRoutes from "footer.routes";
+import Headers from "./components/Headers";
+import Information from "./components/Information";
+import Products from "./components/Products";
+
+function InductionBending() {
+  return (
+    <>
+      <DefaultNavbar routes={routes} transparent light />
+      <Headers />
+      <Card
+        sx={{
+          p: 2,
+          mx: { xs: 2, lg: 3 },
+          mt: -8,
+          mb: 4,
+          boxShadow: ({ boxShadows: { xxl } }) => xxl,
+        }}
+      >
+        <Information />
+        <Products />
+      </Card>
+      <MKBox pt={6} px={1} mt={6}>
+        <DefaultFooter content={footerRoutes} />
+      </MKBox>
+    </>
+  );
+}
+
+export default InductionBending;
